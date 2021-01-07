53cffeaf5ecae9ce9a9c7d70278ea83bb45e1b10
luanhroliveira <luanhigor98@gmail.com>
2021-01-06 22:09:16 -0300

    Ready to prod

--- backend/src/main/java/com/dev/dsdelivery/services/OrderService.java
+++ backend/src/main/java/com/dev/dsdelivery/services/OrderService.java
@@ -31,7 +31,7 @@ public class OrderService {
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
 
-	@Transactional(readOnly = true)
+	@Transactional
 	public OrderDTO insert(OrderDTO dto) {
 		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
 				OrderStatus.PENDING);
